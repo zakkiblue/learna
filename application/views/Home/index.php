50906c045fff3eada5a248564e5d6de1e6887624
zakkiblue <zakkimahator77@gmail.com>
2019-12-02 08:01:14 +0700

    landing page

--- application/views/Home/index.php
+++ application/views/Home/index.php
@@ -23,7 +23,7 @@
                 <li><a class="nav-link" href="#">Fa'Q</a></li>
             </ul>
         </nav>
-        
+
     </header>
     <main>
         <section class="presentation">
@@ -33,19 +33,31 @@
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                 </div>
                 <div class="cta">
-                    <button class="cta-select">Login</button>
-                    <button class="cta-add">Registrasi</button>
+                    <a href="<?= base_url('auth'); ?>"><button class="cta-select">Login</button></a>
+                    <a href="<?= base_url('auth/signup'); ?>"><button class="cta-add">Registrasi</button></a>
                 </div>
 
             </div>
             <div class="cover">
-                <img src="./img/matebook.png" alt="matebook">
+                <img src="<?= base_url('assets/img/landing'); ?>/online_quiz.svg" alt="cover">
+            </div>
+        </section>
+        <section id="about">
+            <div class="background"></div>
+            <div class="image">
+                <img src="<?= base_url('assets/img/landing'); ?>/toga.svg" alt="toga">
+            </div>
+            <div class="text">
+                <h2>About</h2>
+                <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit nemo amet dicta nobis rerum, delectus, animi rem odio cupiditate laboriosam placeat perferendis quisquam voluptatibus consequatur neque modi voluptatum, suscipit dolorum?
+                    Reiciendis, illo, vitae voluptatibus error tenetur autem eaque sapiente, deserunt porro quae nihil ipsa culpa magnam aliquam sequi magni ratione esse eius nam. Quisquam, mollitia inventore repellat est enim assumenda?
+                    Quisquam id culpa sint eaque. Adipisci, ducimus id! Aliquid ea nam numquam officia, facere eveniet quae soluta dolore architecto modi incidunt aperiam corrupti quaerat totam! Inventore aperiam similique voluptatem aut.
+                    Unde quisquam, debitis vero ut, aliquam neque vitae libero voluptates ipsum ad veritatis iusto culpa minima assumenda atque, architecto laborum? Nam consequatur excepturi unde sit facilis commodi adipisci amet qui.
+                    Sint officiis deserunt natus quo doloribus ad quis quisquam dolorum ipsam nemo modi reiciendis, consequatur sapiente, quam id culpa. Vel dicta, iure illo quibusdam assumenda laborum fuga laboriosam quae eum.</div>
             </div>
         </section>
     </main>
-    <img class="big-eclipse" src="<?= base_url('assets/img/landing'); ?>/big-eclipse.svg" alt="eclipse big">
-    <img class="mid-eclipse" src="<?= base_url('assets/img/landing'); ?>/mid-eclipse.svg" alt="eclipse">
-    <img class="small-eclipse" src="./img/small-eclipse.svg" alt="eclipse">
+
 
 </body>
 
